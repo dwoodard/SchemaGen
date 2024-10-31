d21a68c71922029d993fb90f4076b51b87e9183e
Dustin Woodard <dustin.woodard@gmail.com>
2024-10-30 23:53:51 -0600

    Add SchemaGenServiceProvider for service registration and bootstrapping

---
+++ src/SchemaGenServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SchemaGen;
+
+use Illuminate\Support\ServiceProvider;
+
+class SchemaGenServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Register bindings or services here if needed
+    }
+
+    public function boot()
+    {
+        // Set up resources, routes, views, or other services
+    }
+}
